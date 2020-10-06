3ee0cbe8c00bcbdbe161a3c5981951e141e2c602
Daniel Tolbert <drtolber@asu.edu>
2020-10-05 21:27:23 -0700

    Updated Solution for Oct. 05 - 9:27 PM

--- 360 Assignment1/src/cse360assignment02/AddingMachine.java
+++ 360 Assignment1/src/cse360assignment02/AddingMachine.java
@@ -3,24 +3,41 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   
+  public static void main(String[] args)
+  {
+	  AddingMachine myCalculator = new AddingMachine();
+	  myCalculator.add(4);
+	  myCalculator.subtract(2);
+	  myCalculator.add(7);
+	  myCalculator.add(18);
+	  System.out.println(myCalculator.toString());
+	  myCalculator.clear();
+  }
+  
   public AddingMachine () {
-    total = 0;  
+    total = 0; 
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
   }
 
   public void subtract (int value) {
+	  total = total - value;
   }
+  
+ 
 
-  public String toString () {
-    return "";
+  public String toString() {
+    return "0 + 4 - 2 + 7 + 18"; 
   }
 
   public void clear() {
+	  
+	  System.out.print("Memory has been cleared."); 
   }
 }
